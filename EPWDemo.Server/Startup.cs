227ef04824f2f0aa148d535974eed586247dc7b9
Pedro Marques <pedromarques.be@pm.me>
2019-12-18 10:09:44 +0100

    added singleton EPWDB

--- EPWDemo.Server/Startup.cs
+++ EPWDemo.Server/Startup.cs
@@ -31,6 +31,7 @@ namespace EPWDemo.Server
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
             services.AddSingleton<EPWDataFrameBuilder>();
+            services.AddSingleton<EPWDB>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
